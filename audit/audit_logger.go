255c9ac2151190c2136601b2c06bf1b84d8ec673
shadowbq <shadowbq@gmail.com>
2024-09-05 09:23:39 -0400

    updated audit, and log handling in deb packaging

---
+++ audit/audit_logger.go
@@ -0,0 +1,23 @@
+package audit
+
+import (
+	"log"
+	"os"
+)
+
+// Create a logger
+var AuditLogger *log.Logger
+
+// Log a message to the audit log
+func AuditLog(message string) {
+	AuditLogger.Println(message)
+}
+
+func InitAuditLogger() {
+	file, err := os.OpenFile("/var/log/snh-audit.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Failed to open audit log file: %v", err)
+	}
+
+	AuditLogger = log.New(file, "", log.LstdFlags)
+}
